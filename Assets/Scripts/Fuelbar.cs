9008f6ac17155421fa8c74a9fb88c744ee8629ba
tedis6 <travi.hartman@gmail.com>
2025-11-09 16:34:28 +0100

    Items drin

---
+++ Assets/Scripts/Fuelbar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Fuelbar : MonoBehaviour
+{
+    Slider slider;
+    public float fuelTime = 10.0f;
+    float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        slider.value = Mathf.Lerp(1f, 0f, timer / fuelTime);
+
+
+    }
+
+    public void Refill()
+    {
+        timer = 0f;
+        slider.value = 1.0f;
+    }
+}
---
+++ Assets/Scripts/GameManager1.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager1 : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Fuelbar fuelBar;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public bool gameOver = false;
+    public bool isFinished = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuelBar = FindObjectOfType<Fuelbar>();
+        score = 0;
+        scoreText.text = "Score:" + score;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            fuelBar.Refill();
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Coin"))
+        {
+            UpdateScore(5);
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Stop"))
+        {
+            //fuelBar.GameOver();
+            Destroy(other.gameObject);
+            isFinished = true;
+        }
+
+
+    }
+
+    void UpdateScore(int additionalScore)
+    {
+        score += additionalScore;
+        scoreText.text = "Score: " + score;
+
+    }
+}
